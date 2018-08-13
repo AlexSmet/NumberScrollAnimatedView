9733e59fd63f508985feb1e76e5e747a329c3495
Alexander Smetannikov <alexsmetdev@gmail.com>
2018-08-13 09:45:23 +0300

    Classes was splitted to files

---
+++ SHNumbersScrollAnimatedView/ScrollableColumn.swift
@@ -0,0 +1,183 @@
+//
+//  ScrollableColumn.swift
+//  Example
+//
+//  Created by Alexander Smetannikov on 13/08/2018.
+//  Copyright © 2018 Alexander Smetannikov. All rights reserved.
+//
+
+import UIKit
+
+enum ScrollingDirection {
+    case up
+    case down
+}
+
+class ScrollableColumn {
+    var symbol: Character!
+    private var font: UIFont
+    private var textColor: UIColor
+    var timeOffset: CFTimeInterval = 0
+    var duration: CFTimeInterval = 5
+    var durationOffset: CFTimeInterval = 0
+    var scrollingDirection: ScrollingDirection = .down
+    var inverseSequence: Bool = false
+
+    var scrollLayer: CAScrollLayer
+    private var digitCharacter: Character!
+
+    init(withFrame frame: CGRect, forLayer superLayer: CALayer, font: UIFont, textColor: UIColor) {
+        self.font = font
+        self.textColor = textColor
+
+        scrollLayer = CAScrollLayer()
+        scrollLayer.frame = frame
+        superLayer.addSublayer(scrollLayer)
+    }
+
+    func createAnimation(timeOffset: CFTimeInterval, duration: CFTimeInterval, durationOffset: CFTimeInterval, scrollingDirection: ScrollingDirection, inverseSequence: Bool = false) {
+        self.timeOffset = timeOffset
+        self.duration = duration
+        self.durationOffset = durationOffset
+        self.scrollingDirection = scrollingDirection
+        self.inverseSequence = inverseSequence
+
+        if let _ = Int(String(symbol)) {
+            createContent(numericalSymbol: symbol)
+            addBeginAnimation()
+            addMainAnimation()
+        } else {
+            createContent(nonNumericalSymbol: symbol)
+        }
+    }
+
+    private func addBeginAnimation() {
+        let animation = CABasicAnimation(keyPath: "sublayerTransform.translation.y")
+        animation.duration = timeOffset
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        animation.fromValue = getStartPositionYForAnimation()
+        animation.toValue = (animation.fromValue as! CGFloat)
+
+        scrollLayer.add(animation, forKey: nil)
+    }
+
+    private func addMainAnimation() {
+        let animation = CABasicAnimation(keyPath: "sublayerTransform.translation.y")
+        animation.beginTime = CACurrentMediaTime() + timeOffset
+        animation.duration = duration - durationOffset
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        animation.fromValue = getStartPositionYForAnimation()
+        animation.toValue = 0
+
+        scrollLayer.add(animation, forKey: nil)
+    }
+
+    private func getStartPositionYForAnimation() -> CGFloat {
+        var result: CGFloat
+
+        let startOffsetY =  scrollLayer.frame.height
+        switch scrollingDirection {
+        case .down:
+            let maxY: CGFloat = (scrollLayer.sublayers?.last?.frame.origin.y)!
+            result  = -maxY - startOffsetY
+        case .up:
+            let minY: CGFloat = (scrollLayer.sublayers?.first?.frame.origin.y)!
+            result = -minY + startOffsetY
+        }
+
+        return result
+    }
+
+    func removeAllAnimations() {
+        scrollLayer.removeAllAnimations()
+    }
+
+    func removeFromSuperlayer() {
+        scrollLayer.removeFromSuperlayer()
+    }
+
+    private func createContent(nonNumericalSymbol: Character) {
+        let textLayer = createTextLayer(withText: String(nonNumericalSymbol))
+        textLayer.frame = CGRect(x: 0, y: 0, width: scrollLayer.frame.width, height: scrollLayer.frame.height)
+        scrollLayer.addSublayer(textLayer)
+    }
+
+    private func createContent(numericalSymbol: Character) {
+        let number = Int(String(numericalSymbol))!
+        var textForScroll = [String]()
+
+        var firstNumber: Int
+        var lastNumber: Int
+        var by: Int = 1
+
+        // Scrolling starts from 0, by default. But when a target value is 0, than we can start scrolling from 1 or 9 (depends on the values of scrollingDirection and inverseSequence)
+        switch scrollingDirection {
+        case .up:
+            lastNumber = number
+            if inverseSequence {
+                by = -1
+                firstNumber = 10
+                if number == 0 {
+                    firstNumber = 9
+                }
+            } else {
+                firstNumber = 0
+                if number == 0 {
+                    firstNumber = 1
+                    lastNumber = 10
+                }
+            }
+        case .down:
+            firstNumber = number
+            if inverseSequence {
+                by = -1
+                lastNumber = 0
+                if number == 0 {
+                    firstNumber = 10
+                    lastNumber = 1
+                }
+
+            } else {
+                lastNumber = 10
+                if number == 0 {
+                    lastNumber = 9
+                }
+            }
+        }
+
+        for i in stride(from: firstNumber, through: lastNumber, by: by) {
+            textForScroll.append("\(i%10)")
+        }
+
+        var height: CGFloat = 0
+        scrollLayer.sublayers?.removeAll()
+
+        switch scrollingDirection {
+        case .down:
+            height = 0
+        case .up:
+            height = -scrollLayer.frame.height * CGFloat(textForScroll.count-1)
+        }
+
+        textForScroll.forEach {
+            let textLayer = createTextLayer(withText: $0)
+            textLayer.frame = CGRect(x: 0, y: height, width: scrollLayer.frame.width, height: scrollLayer.frame.height)
+            scrollLayer.addSublayer(textLayer)
+            height += scrollLayer.frame.height
+        }
+    }
+
+    private func createTextLayer(withText: String) -> VerticallyCenteredTextLayer {
+        let newLayer = VerticallyCenteredTextLayer()
+        let attributedString = NSAttributedString(
+            string: withText,
+            attributes: [
+                NSAttributedStringKey.foregroundColor: textColor.cgColor,
+                NSAttributedStringKey.font: font
+            ])
+        newLayer.alignmentMode = kCAAlignmentCenter
+        newLayer.string = attributedString
+
+        return newLayer
+    }
+}
